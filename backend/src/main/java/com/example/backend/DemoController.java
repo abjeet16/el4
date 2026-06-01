e86b776036e0d24cbdae60e080ab4c6f31e61716
abjeet <avjeetyadav00@gmail.com>
2026-06-01 22:34:56 +0530

    done

---
+++ backend/src/main/java/com/example/backend/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.backend;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/hello")
+    @CrossOrigin("*")
+    public String sayHello(){
+        return "hello from backend";
+    }
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+import { useEffect, useState } from "react";
+
+function App() {
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/hello")
+        .then((response) => response.text())
+        .then((data) => setMessage(data))
+        .catch((error) => console.error(error));
+  }, []);
+
+  return (
+      <div>
+        <h1>React Frontend</h1>
+        <h2>{message}</h2>
+      </div>
+  );
+}
+
+export default App;
